8fc4c2d56c727e6ab55e2966e044f13e5981693c
JuanDavidCortesOssa <64558296+JuanDavidCortesOssa@users.noreply.github.com>
2025-02-26 17:49:16 -0500

    first files

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,9 @@
+<template>
+
+<div class = "text-center my-24">
+    <h1 class = "text-4x1 font-bold traching-widest text-sky-700">
+        Enron Email</h1>
+    <h6>A simple app to search emails</h6>
+</div class = "mt-4 text-x1">
+
+</template>
